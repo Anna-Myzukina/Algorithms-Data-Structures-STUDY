5c8f5d06536fd952732ff607ed427943cc395871
Anna <anna.muzykina83@gmail.com>
2019-08-08 11:26:16 +0300

    Create экспонента.rb

---
+++ экспонента.rb
@@ -0,0 +1,11 @@
+=begin
+https://ru.wikipedia.org/wiki/%D0%AD%D0%BA%D1%81%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D0%B0
+
+Экспонента и её смысл:
+https://www.youtube.com/watch?v=rQaCkivg-NM
+
+https://ruby-doc.org/core-2.5.3/Math.html
+http://qaru.site/questions/1035787/large-exponents-in-ruby
+https://ru.wikibooks.org/wiki/Ruby/%D0%A1%D0%BF%D1%80%D0%B0%D0%B2%D0%BE%D1%87%D0%BD%D0%B8%D0%BA/Math
+
+=end
