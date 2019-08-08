8030026771bba4a39001cda579c0291e4aad99d5
Anna <anna.muzykina83@gmail.com>
2019-08-08 11:22:23 +0300

    Create доказательство по индукции.rb

---
+++ доказательство по индукции.rb
@@ -0,0 +1,6 @@
+=begin
+https://www.youtube.com/watch?v=X_KXgYFCmz0
+http://www.math.md/school/krujok/inductr/inductr.html
+https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D0%B8%D0%BD%D0%B4%D1%83%D0%BA%D1%86%D0%B8%D1%8F
+
+=end
